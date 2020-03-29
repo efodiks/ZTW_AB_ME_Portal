cd085ae072a933b920ddf57cfa843968bf723fc3
efodiks <michallengel@gmail.com>
2020-03-29 23:30:27 +0200

    redux first draft

--- front/portal/src/App.js
+++ front/portal/src/App.js
@@ -4,19 +4,22 @@ import NavigationBar from './components/layout/NavigationBar.jsx';
 import Login from './components/login/Login.jsx';
 import Registration from './components/registration/Registration.jsx';
 import Dashboard from './components/dashboard/Dashboard.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Provider} from "react-redux";
 
-function App() {
-  return (
-    <Router>
-      <NavigationBar/>
-      <Switch>
-        <Route path="/dashboard" component={Dashboard}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/" component={Registration}/>
-      </Switch>
-    </Router>
-  );
+function App({store}) {
+    return (
+        <Provider store={store}>
+            <Router>
+                <NavigationBar/>
+                <Switch>
+                    <Route path="/dashboard" component={Dashboard}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/" exact component={Registration}/>
+                </Switch>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
--- front/portal/src/components/login/Login.jsx
+++ front/portal/src/components/login/Login.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import LoginForm from './LoginForm.jsx';
-import { Container, Row, Col } from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
+import {connect} from "react-redux";
 
-const Login = () => {
+const mapStateToProps = state => {
+    return {
+        firstName: state.user.firstName,
+        lastName: state.user.lastName,
+        token: state.token
+    }
+};
 
+const Login = ({firstName, lastName, token}) => {
     return (
         <Container>
             <Row className="justify-content-center">
-                    <Col lg={6}>
-                        <LoginForm/>   
-                    </Col>
+                <Col lg={6}>
+                    <div>`Stan: {firstName}, {lastName}`</div>
+                    <LoginForm/>
+                </Col>
             </Row>
         </Container>
     );
-}
+};
 
-export default Login;
+export default connect(mapStateToProps)(Login);
