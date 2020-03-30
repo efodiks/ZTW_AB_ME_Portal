7d1298a6f1db587c26e91606eeb26b4f6c875d41
adobek <45313672+adobek@users.noreply.github.com>
2020-03-30 15:42:51 +0200

    Form to add posts

--- front/portal/src/components/dashboard/AddPost.jsx
+++ front/portal/src/components/dashboard/AddPost.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Card } from 'react-bootstrap';
+import { Container, Row, Card, Form, Col, Button } from 'react-bootstrap';
 
 const AddPost = () => {
 
@@ -7,9 +7,30 @@ const AddPost = () => {
         <Container>
             <Row className="justify-content-center" style={{margin:"1.5em 0em 1.5em 0em"}}>
                 <Card body className="w-100">
-                    <Card.Title>
-                        Add post
-                    </Card.Title>
+                    <h1>Add post</h1>
+                    <Form >
+                        <Form.Row style={{marginTop: "1em"}}>
+                            <Form.Label column lg={2}>
+                                Picture URL
+                            </Form.Label>
+                            <Col>
+                                <Form.Control type="text" placeholder="URL" />
+                            </Col>
+                        </Form.Row>
+                        <Form.Row style={{marginTop: "1em", marginBottom: "1em"}}>
+                            <Form.Label column lg={2}>
+                                Description
+                            </Form.Label>
+                            <Col>
+                                <Form.Control as="textarea" placeholder="Description..." />
+                            </Col>
+                        </Form.Row>
+                        <div align="right">
+                            <Button id="addPost" variant="info" type="submit" style={{width:"30%"}}>
+                                Add post
+                            </Button>
+                        </div>
+                    </Form>
                 </Card>
             </Row>
         </Container>
