7e44028e703d68d5c08aecdff18e7eef6991a243
Michal Engel <michallengel@gmail.com>
2020-04-23 19:44:13 +0200

    Added posts boostraping

--- src/main/java/com/abme/portal/DataBootstrap.java
+++ src/main/java/com/abme/portal/DataBootstrap.java
@@ -2,15 +2,17 @@ package com.abme.portal;
 
 
 import com.abme.portal.domain.Authority;
-import com.abme.portal.dto.PostDTO;
+import com.abme.portal.domain.Post;
 import com.abme.portal.dto.UserDTO;
 import com.abme.portal.repository.AuthorityRepository;
-import com.abme.portal.rest.PostController;
+import com.abme.portal.repository.PostRepository;
+import com.abme.portal.repository.UserRepository;
 import com.abme.portal.security.AuthoritiesConstants;
 import com.abme.portal.security.UserDetailsService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 @Component
@@ -20,18 +22,31 @@ public class DataBootstrap implements CommandLineRunner {
 
     private final UserDetailsService userDetailsService;
 
-    public DataBootstrap(AuthorityRepository authorityRepository, UserDetailsService userDetailsService) {
-        this.authorityRepository = authorityRepository;
+    private final UserRepository userRepository;
 
+    private final PostRepository postRepository;
+
+    public DataBootstrap(AuthorityRepository authorityRepository, UserDetailsService userDetailsService, UserRepository userRepository, PostRepository postRepository) {
+        this.authorityRepository = authorityRepository;
         this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(String... args) {
+        bootstrapAuthorities();
+        bootstrapUsers();
+        bootStrapPosts();
+    }
+
+    private void bootstrapAuthorities() {
         Stream.of(AuthoritiesConstants.ANONYMOUS, AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN)
                 .map(Authority::new)
                 .forEach(authorityRepository::save);
+    }
 
+    private void bootstrapUsers() {
         var user1 = new UserDTO();
         user1.setUsername("user1");
         user1.setEmail("user1@user1.com");
@@ -46,5 +61,28 @@ public class DataBootstrap implements CommandLineRunner {
 
         userDetailsService.registerUser(user1);
         userDetailsService.registerUser(user2);
+    }
+
+    private void bootStrapPosts() {
+        var user1 = userRepository.findOneByEmailIgnoreCase("user1@user1.com").orElseThrow();
+
+        var post1User1 = new Post();
+        post1User1.setDescription("Kitty");
+        post1User1.setURL("https://www.4yourspot.com/wp-content/uploads/2019/10/daily-cat-care-routine.jpg");
+        post1User1.setAuthor(user1);
+
+        var post2User1 = new Post();
+        post2User1.setDescription("post nr 1");
+        post2User1.setURL("https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500");
+        post2User1.setAuthor(user1);
+
+        var user2 = userRepository.findOneByEmailIgnoreCase("user2@user2.com").orElseThrow();
+
+        var post1User2 = new Post();
+        post1User2.setDescription("Another kitty");
+        post1User2.setURL("https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg");
+        post1User2.setAuthor(user2);
+
+        postRepository.saveAll(List.of(post1User1, post2User1, post1User2));
     }
 }
